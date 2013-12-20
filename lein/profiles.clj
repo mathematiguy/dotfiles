62e0be2e3f9b4736df5cb28d00208d5da67f767e
roryk <rory.kirchner@gmail.com>
2013-12-20 00:11:10 -0500

    Added lein profile.

---
+++ lein/profiles.clj
@@ -0,0 +1,6 @@
+{:user {:plugins [[lein-difftest "1.3.8"]
+                  [lein-marginalia "0.7.1"]
+                  [lein-pprint "1.1.1"]
+                  [lein-swank "1.4.4"]
+                  [lein-midje "3.0.0"]
+                  [lein-midje-lazytest "0.1.0"]]}}
