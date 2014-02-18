16b83aa81272bd1fca394967d7373919d0a32a45
roryk <rory.kirchner@gmail.com>
2014-02-18 02:53:11 -0500

    Add lein-exec and lein-light.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,5 +2,6 @@
                   [lein-marginalia "0.7.1"]
                   [lein-pprint "1.1.1"]
                   [lein-swank "1.4.4"]
-                  [lein-midje "3.0.0"]
-                  [lein-midje-lazytest "0.1.0"]]}}
+                  [lein-midje "3.1.3"]
+                  [lein-exec "0.3.1"]
+		  [lein-light "0.0.8"]]}}
