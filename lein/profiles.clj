57b28dd28348ee3a505ee1c20e570a1d061087d1
roryk <rory.kirchner@gmail.com>
2014-05-18 01:45:07 -0400

    Clean imports, update with slamhound.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,8 @@
-{:user {:plugins [[lein-difftest "1.3.8"]
-                  [lein-marginalia "0.7.1"]
-                  [lein-pprint "1.1.1"]
-                  [lein-swank "1.4.4"]
-                  [lein-midje "3.1.3"]
-                  [lein-exec "0.3.1"]
-		  [lein-light "0.0.8"]]}}
+{:user
+ {:plugins [[lein-pprint "1.1.1"]
+            [lein-midje "3.1.3"]
+            [lein-exec "0.3.1"]
+	    [cider/cider-nrepl "0.7.0-SNAPSHOT"]]}
+ {:dependencies [[spyscope "0.1.4"]
+                 [slamound "1.5.5"]]}
+ {:injections [(require 'spyscope.core)]}}
