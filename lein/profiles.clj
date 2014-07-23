c92707e206d911f70e6bdb8ea7065fa931e5533d
roryk <rory.kirchner@gmail.com>
2014-07-23 15:54:03 -0400

    Add lein-exec, lein-try and update CIDER.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,8 +1,11 @@
 {:user
  {:plugins [[lein-pprint "1.1.1"]
             [lein-midje "3.1.3"]
-            [lein-exec "0.3.1"]
-	    [cider/cider-nrepl "0.7.0-SNAPSHOT"]]}
- {:dependencies [[spyscope "0.1.4"]
-                 [slamound "1.5.5"]]}
- {:injections [(require 'spyscope.core)]}}
+            [lein-try "0.4.1"]
+	    [lein-exec "0.3.1"]
+	    [cider/cider-nrepl "0.7.0-SNAPSHOT"]]
+  :dependencies [[spyscope "0.1.4"]
+                 [slamhound "1.5.5"]]
+  :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+  :injections [(require 'spyscope.core)]}}
+
