b88c67e99b5182b4db409beb6e5e2e139a497d41
roryk <rory.kirchner@gmail.com>
2014-08-20 18:06:32 -0400

    Remove extraneous packages.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,9 +3,5 @@
             [lein-midje "3.1.3"]
             [lein-try "0.4.1"]
 	    [lein-exec "0.3.1"]
-	    [cider/cider-nrepl "0.7.0-SNAPSHOT"]]
-  :dependencies [[spyscope "0.1.4"]
-                 [slamhound "1.5.5"]]
-  :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-  :injections [(require 'spyscope.core)]}}
-
+	    [cider/cider-nrepl "0.7.0"]]
+  :dependencies [[spyscope "0.1.4"]]}}
