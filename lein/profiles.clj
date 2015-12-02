701a903fcc9a92d0d687e8699404ab8e49578ab7
roryk <rory.kirchner@gmail.com>
2015-12-02 18:32:54 -0500

    Simplify leiningen profile.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,10 +1,2 @@
-{:user
- {:plugins [[lein-pprint "1.1.1"]
-            [venantius/ultra "0.2.0"]
-            [lein-midje "3.1.3"]
-            [lein-try "0.4.1"]
-            [lein-exec "0.3.1"]
-            [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-            [com.jakemccrary/lein-test-refresh "0.6.0"]
-            [refactor-nrepl "1.0.5"]]
-  :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.9.1"]]}}
+
