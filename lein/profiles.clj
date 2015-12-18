407a6de948573e6525f27f67e52c6ffc2d15ced9
roryk <rory.kirchner@gmail.com>
2015-12-18 01:43:28 -0500

    Move to newest versions of CIDER et. al.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]]}}
-
+{:user
+ {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+            [refactor-nrepl "2.0.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
+{:repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
