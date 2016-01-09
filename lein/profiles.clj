78156f7777fc6f4b3ed76fd6b9ca0e433e0dc7c9
roryk <rory.kirchner@gmail.com>
2016-01-09 18:44:48 -0500

    Remove nrepl from normal dependencies.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,4 @@
 {:user
  {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-            [refactor-nrepl "2.0.0-SNAPSHOT"]]
-  :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
+            [refactor-nrepl "2.0.0-SNAPSHOT"]]}}
 {:repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
