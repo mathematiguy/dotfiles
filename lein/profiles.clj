922cf5fd7d23e4a24ad261d1389ce516174e3479
roryk <rory.kirchner@gmail.com>
2016-08-28 11:09:08 -0400

    Bump cider-nrepl version.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,2 @@
-{:user {:plugins [[cider/cider-nrepl "0.13.0-SNAPSHOT"]
-                  [refactor-nrepl "2.3.0-SNAPSHOT"]]
+{:user {:plugins [[cider/cider-nrepl "0.14.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
